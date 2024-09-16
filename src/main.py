3fb189fabd5e0675cc349e6688fad5c363b1982c
Jason Reilly <jdr0887@gmail.com>
2024-09-16 11:43:09 -0400

    slightly more dynamic, but error prone as what actually is in the data may not parse properly (eg dateTime vs date)

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import json
 import pathlib
 import logging
 import sys
+
 import rdflib
-from rdflib import Namespace, Graph, XSD
+import yaml
+from rdflib import Namespace, Graph, XSD, IdentifiedNode, URIRef
+from rdflib.extras.describer import cast_value, Describer
 from rdflib.namespace import NamespaceManager
 
 logger = logging.getLogger(__name__)
@@ -18,71 +21,94 @@ logging.basicConfig(
     ]
 )
 
-def main(input: pathlib.Path, output: pathlib.Path):
+def main(input: pathlib.Path, conf: pathlib.Path, output: pathlib.Path):
     logger.info(f"input: {input}, output: {output}")
 
-    wokn = Namespace("https://wildlife.proto-okn.net/kg/")
+    with open(input, 'r') as file, open(conf, 'r') as conf:
+        conf_yaml = yaml.safe_load(conf)
+        base = conf_yaml['base']
 
-    namespace_manager = NamespaceManager(Graph())
-    namespace_manager.bind('wokn', wokn, override=False)
+        base_namespace = Namespace(base['iri'])
 
-    g = Graph(base="https://wildlife.proto-okn.net/kg/")
-    g.namespace_manager = namespace_manager
+        namespace_manager = NamespaceManager(Graph())
+        namespace_manager.bind(base['prefix'], base_namespace, override=False)
 
-    with open(input, 'r') as file:
+        g = Graph(base=base['iri'])
+        g.namespace_manager = namespace_manager
+
+        # print(g.serialize(format="turtle"))
+
+        mappings = conf_yaml['mappings']
+
         for line in file:
             value = json.loads(line)
             id = value["id"]
             t = value["type"]
-            # logger.info(f"id: {id}, type: {t}")
+            logger.debug(f"id: {id}, type: {t}")
+            properties = value["properties"]
             if t == "node":
-                property_name = value["properties"]["name"]
-                property_name = str(property_name).replace('\n', '')
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDFS.label, rdflib.Literal(property_name, datatype=XSD.string)))
+                for mapping in mappings:
+                    if mapping in properties:
+                        property_mapping = properties[mapping]
+                        property_mapping = str(property_mapping).replace('\n', '')
+                        g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mappings[mapping]['iri']), rdflib.Literal(property_mapping, datatype=URIRef(mappings[mapping]['type']))))
+
                 labels = value["labels"]
                 for label in labels:
-                    g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDF.type, rdflib.term.URIRef(label, wokn)))
+                    g.add((rdflib.term.URIRef(id, base_namespace), rdflib.namespace.RDF.type, rdflib.term.URIRef(label, base_namespace)))
 
             if t == "relationship":
                 label = value["label"]
-
-                properties_multimedia = value["properties"]["multimedia"]
-                properties_dates = value["properties"]["dates"]
-                properties_observed_times = value["properties"]["observed_times"]
-
                 start_id = value["start"]["id"]
-                # start_labels = value["start"]["labels"]
-                # start_properties = value["start"]["properties"]
-                # start_properties_name = value["start"]["properties"]["name"]
-
                 end_id = value["end"]["id"]
-                # end_labels = value["end"]["labels"]
-                # start_properties = value["end"]["properties"]
-                # start_properties_name = value["end"]["properties"]["name"]
+                g.add((rdflib.term.URIRef(start_id, base_namespace), rdflib.term.URIRef(label, base_namespace), rdflib.term.URIRef(end_id, base_namespace)))
+                g.add((rdflib.term.URIRef(id, base_namespace), rdflib.namespace.RDF.subject, rdflib.term.URIRef(start_id, base_namespace)))
+                g.add((rdflib.term.URIRef(id, base_namespace), rdflib.namespace.RDF.predicate, rdflib.term.URIRef(label, base_namespace)))
+                g.add((rdflib.term.URIRef(id, base_namespace), rdflib.namespace.RDF.object, rdflib.term.URIRef(end_id, base_namespace)))
+                g.add((rdflib.term.URIRef(id, base_namespace), rdflib.namespace.RDF.type, rdflib.namespace.RDF.Statement))
 
-                g.add((rdflib.term.URIRef(start_id, wokn), rdflib.term.URIRef(label, wokn), rdflib.term.URIRef(end_id, wokn)))
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDF.type, rdflib.namespace.RDF.Statement))
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDF.subject, rdflib.term.URIRef(start_id, wokn)))
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDF.predicate, rdflib.term.URIRef(label, wokn)))
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.RDF.object, rdflib.term.URIRef(end_id, wokn)))
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.SDO.subjectOf, rdflib.term.URIRef(f"{properties_multimedia}")))
-                if "T" in properties_dates:
-                    g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.DCTERMS.date, rdflib.Literal(properties_dates, datatype=XSD.dateTime)))
-                else:
-                    g.add((rdflib.term.URIRef(id, wokn), rdflib.namespace.DCTERMS.date, rdflib.Literal(properties_dates, datatype=XSD.date)))
+                for mapping_key, mapping_value in mappings.items():
+                    try:
 
-                g.add((rdflib.term.URIRef(id, wokn), rdflib.term.URIRef("observed_times", wokn), rdflib.Literal(properties_observed_times, datatype=XSD.dateTime)))
+                        if not mapping_key in properties:
+                            continue
 
-                # break
+                        property_mapping_value = properties[mapping_key]
+                        logger.debug(f"mapping: {mapping_value}, value: {property_mapping_value}, value type: {type(property_mapping_value)}")
 
-    g.serialize(destination=output)
+                        if 'iri' not in mapping_value:
+                            g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mapping, base_namespace), rdflib.Literal(property_mapping_value, datatype=URIRef(mapping_value['type']))))
+                        else:
+
+                            if mapping_value['type'] == 'IRI':
+
+                                g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mapping_value['iri']), rdflib.term.URIRef(f"{property_mapping_value}")))
+
+                            else:
+
+                                if rdflib.XSD.dateTime.eq(URIRef(mapping_value['type'])):
+
+                                    if "T" in property_mapping_value:
+                                        g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mapping_value['iri']), rdflib.Literal(property_mapping_value, datatype=rdflib.XSD.dateTime)))
+                                    else:
+                                        g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mapping_value['iri']), rdflib.Literal(property_mapping_value, datatype=XSD.date)))
+
+                                else:
+
+                                    g.add((rdflib.term.URIRef(id, base_namespace), URIRef(mapping_value['iri']), rdflib.Literal(property_mapping_value, datatype=URIRef(mapping_value['type']))))
+                    except:
+                        logger.exception("error")
+
+        g.serialize(destination=output)
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='neo4j json-ld to ttl')
     parser.add_argument('-i', '--input', required=True, type=pathlib.Path, help='A json-ld file from a Neo4j export')
+    parser.add_argument('-c', '--conf', required=True, type=pathlib.Path, help='One of the yaml files found in conf')
     parser.add_argument('-o', '--output', required=True, type=pathlib.Path, help='A Turtle file')
 
     args = parser.parse_args()
 
-    main(args.input, args.output)
+    main(args.input, args.conf, args.output)
