47f1cdf06ba05a7d197e797addd142f8937ab0d4
Lisa Stillwell <lisa@renci.org>
2025-02-21 11:43:03 -0500

    added new logic to handle biohealth data correctly

--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ import sys
 
 import rdflib
 import yaml
-from rdflib import Namespace, Graph, XSD, IdentifiedNode, URIRef
-# from rdflib.extras.describer import cast_value, Describer
-# from rdflib.namespace import NamespaceManager
+from rdflib import Namespace, Graph, XSD, URIRef
+from flatten_json import flatten
 
 logger = logging.getLogger(__name__)
 # logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -21,13 +20,46 @@ logging.basicConfig(
     ]
 )
 
+def  isURI(str):
+    iri_prefixes = ['http:', 'https:', 'urn:', 'mailto:']
+    if str:
+        return False
+    else:
+        return (str.startswith(tuple(iri_prefixes)))
+
+def create_node_id_mapping(base, node_namspace, id, mappings, mapping, property_mapping, prop_identifiers):
+
+    node_id_mapping = {}
+    # set up default
+    node_id_mapping = {'iri': id, 'namespace': node_namspace}
+
+    # change iri, if needed  (type = IRI or CURIE)
+    # if iri gets changed, must save mapping from original id to new iri
+    if 'type' in mappings[mapping] and mappings[mapping]['type'] in ['IRI', 'CURIE']:
+        # check if it’s a CURIE using one of the defined prefixes
+        if mapping in prop_identifiers:
+            if isURI(property_mapping):
+                node_id_mapping['iri'] = property_mapping
+                node_id_mapping['namespace'] = property_mapping
+            else: # it is a bare id
+                # check to see if there is a base for this property value
+                # if so, append it to that
+                if 'base' in mappings[mapping]:
+                    node_id_mapping['namespace'] = mappings[mapping]['base']
+                    node_id_mapping['iri'] = node_id_mapping['namespace'] + property_mapping
+                # if not, append it to the global base value
+                else:
+                    node_id_mapping['iri'] = base + property_mapping
+                    node_id_mapping['namespace'] = base
+
+    return node_id_mapping
+
 def main(input: pathlib.Path, conf: pathlib.Path, output: pathlib.Path):
     logger.info(f"input: {input}, output: {output}")
 
     with open(input, 'r') as file, open(conf, 'r') as conf:
         conf_yaml = yaml.safe_load(conf)
         base = conf_yaml['base']
-        # base_namespace = Namespace(base)
         schema_namespace = Namespace(base + "schema/")
         node_namespace = Namespace(base + "node/")
         relationship_namespace = Namespace(base + "relationship/")
@@ -41,6 +73,10 @@ def main(input: pathlib.Path, conf: pathlib.Path, output: pathlib.Path):
 
         # print(g.serialize(format="turtle"))
 
+        node_id_mappings = {}
+        prop_identifiers = []
+        if 'identifier_properties' in conf_yaml:
+            prop_identifiers = conf_yaml['identifier_properties']
         mappings = conf_yaml['mappings']
 
         for line in file:
@@ -54,37 +90,57 @@ def main(input: pathlib.Path, conf: pathlib.Path, output: pathlib.Path):
                 properties = value["properties"]
             else:
                 properties = {}
+            properties = flatten(properties)
 
             if t == "node":
-                # construct iri for node
-                iri = id
-                if "identifier" in properties:
-                    iri = properties["identifier"]
 
                 for mapping in mappings:
                     if mapping in properties:
                         property_mapping = properties[mapping]
                         property_mapping = str(property_mapping).replace('\n', '')
-
-                        if 'iri' not in mappings[mapping]:
-                            mappings[mapping]['iri'] = schema_namespace + mapping
-                        g.add((rdflib.term.URIRef(iri, node_namespace), URIRef(mappings[mapping]['iri']), rdflib.Literal(property_mapping, datatype=URIRef(mappings[mapping]['type']))))
+                        if not id in node_id_mappings:
+                            node_id_mappings[id] = create_node_id_mapping(base, node_namespace, id, mappings, mapping, property_mapping, prop_identifiers)
+                        if mapping not in prop_identifiers:
+                            if mappings[mapping]['type'] == "IRI":
+                                if 'iri' in mappings[mapping]:
+                                    predicate = mappings[mapping]['iri']
+                                else:
+                                    predicate = node_id_mappings[id]['iri']
+                                if 'base' in mappings[mapping]:
+                                    literal = mappings[mapping]['base'] + properties[mapping]
+                                else:
+                                    literal = property_mapping
+                                g.add((rdflib.term.URIRef(node_id_mappings[id]['iri'], node_id_mappings[id]['namespace']), URIRef(predicate), URIRef(literal)))
+                            else:
+                                if 'iri' in mappings[mapping]:
+                                    uriref = mappings[mapping]['iri']
+                                else:
+                                    uriref = schema_namespace
+                                g.add((rdflib.term.URIRef(node_id_mappings[id]['iri'], node_id_mappings[id]['namespace']), URIRef(uriref), rdflib.Literal(property_mapping, datatype=URIRef(mappings[mapping]['type']))))
 
                 labels = value["labels"]
                 for label in labels:
-                    g.add((rdflib.term.URIRef(iri, node_namespace), rdflib.namespace.RDF.type, rdflib.term.URIRef(label, schema_namespace)))
+                    # set default namespace
+                    label_namespace = schema_namespace
+                    if label in mappings and 'iri' in mappings[label]:
+                        label_namespace = mappings[label]['iri']
+                    g.add((rdflib.term.URIRef(node_id_mappings[id]['iri'], node_id_mappings[id]['namespace']), rdflib.namespace.RDF.type, rdflib.term.URIRef(label, label_namespace)))
 
             if t == "relationship":
 
                 label = value["label"]
+                label_namespace = schema_namespace
+                if label in mappings and 'iri' in mappings[label]:
+                    label_namespace = ""
+                    label = mappings[label]['iri']
                 start_id = value["start"]["id"]
                 end_id = value["end"]["id"]
                 rel_id = id
 
-                g.add((rdflib.term.URIRef(start_id, node_namespace), rdflib.term.URIRef(label, schema_namespace), rdflib.term.URIRef(end_id, node_namespace)))
-                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.subject, rdflib.term.URIRef(start_id, node_namespace)))
-                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.predicate, rdflib.term.URIRef(label, schema_namespace)))
-                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.object, rdflib.term.URIRef(end_id, node_namespace)))
+                g.add((rdflib.term.URIRef(node_id_mappings[start_id]['iri'], node_id_mappings[start_id]['namespace']), rdflib.term.URIRef(label, label_namespace), rdflib.term.URIRef(node_id_mappings[end_id]['iri'], node_id_mappings[end_id]['namespace'])))
+                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.subject, rdflib.term.URIRef(node_id_mappings[start_id]['iri'], node_id_mappings[start_id]['namespace'])))
+                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.predicate, rdflib.term.URIRef(label, label_namespace)))
+                g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.object, rdflib.term.URIRef(node_id_mappings[end_id]['iri'], node_id_mappings[end_id]['namespace'])))
                 g.add((rdflib.term.URIRef(rel_id, relationship_namespace), rdflib.namespace.RDF.type, rdflib.namespace.RDF.Statement))
 
                 for mapping_key, mapping_value in mappings.items():
